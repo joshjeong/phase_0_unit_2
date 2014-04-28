6f631a6fca33aa9c1db3799b7e75e809df5d02d1
joshjeong <jcjeong@gmail.com>
2014-04-27 20:31:59 -0700

    Update my_solution.rb

--- week_4/1_mathy_ruby_intro/medium_calculate_letter_grade/my_solution.rb
+++ week_4/1_mathy_ruby_intro/medium_calculate_letter_grade/my_solution.rb
@@ -4,7 +4,7 @@
 # include it in this file. Also make sure everything that isn't code
 # is commented in the file.  
 
-# I worked on this challenge [by myself, with: ].
+# I worked on this challenge [by myself].
 
 # 1. Pseudocode
 
@@ -12,6 +12,11 @@
 # What is the output? (i.e. What should the code return?)
 # What are the steps needed to solve the problem?
 
+# def get_grade method with single argument of an array of scores
+# set sum to zero
+# iterate through each score and add to sum
+# find average of all scores and assign it a var
+# create letter grade conditions
 
 # 2. Initial Solution
 
@@ -35,14 +40,24 @@ def get_grade(scores)
 	end
 end
 
-			
-			
-			
-			
-
 
 # 3. Refactored Solution
 
 
 
 # 4. Reflection 
+
+What parts of your strategy worked? What problems did you face?
+After writing the pseudo, the coding was pretty straightforward 
+What questions did you have while coding? What resources did you find to help you answer them?
+None, I think we did a similar question in the prep course exercises.
+What concepts are you having trouble with, or did you just figure something out? If so, what?
+NA
+Did you learn any new skills or tricks?
+NA
+How confident are you with each of the Learning Competencies?
+Straight chillin
+Which parts of the challenge did you enjoy?
+The part where I got an A
+Which parts of the challenge did you find tedious?
+Nah
