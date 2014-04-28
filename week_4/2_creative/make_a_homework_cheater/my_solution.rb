2d983f1b9a605d76ae587bacdf0988f231efd91f
joshjeong <jcjeong@gmail.com>
2014-04-27 20:59:38 -0700

    Update my_solution.rb

--- week_4/2_creative/make_a_homework_cheater/my_solution.rb
+++ week_4/2_creative/make_a_homework_cheater/my_solution.rb
@@ -1,18 +1,21 @@
 # U2.W4: Homework Cheater!
 
 
-# I worked on this challenge [by myself, with: ].
+# I worked on this challenge [by myself].
 
 # 2. Pseudocode
 
 # Input:
 # Output:
 # Steps:
+# def method essay_writer
+# pass several arguments: title, person, place, topic, date, thesis
+# create a template and use interpolation to call argument
 
 
 # 3. Initial Solution
 def essay_writer(title, person, place, topic, date, thesis)
-  print "\t #{title}: The life of #{person} can be described as impactful. #{topic} was changed \n forever and a legend was created. Unfortunately, #{person}'s life was cut short on #{date} \n in #{place}. #{thesis}" 
+  print "\t #{title}: The life of #{person} can be described as impactful. #{topic} was changed forever and a legend was created. Unfortunately, #{person}'s life was cut short on #{date} in #{place}. #{thesis}" 
 end
 essay_writer("Father of Rap: 2Pac","Tupac Shakur", "Las Vegas","Hip Hop","1996","He changed an era with his lyrics and actions altering the course of the industry.")
 
@@ -20,7 +23,7 @@ essay_writer("Father of Rap: 2Pac","Tupac Shakur", "Las Vegas","Hip Hop","1996",
 # 4. Refactored Solution
 
 
-
+Father of Rap: 2Pac: The life of Tupac Shakur can be described as impactful. Hip Hop was changed forever and a legend was created. Unfortunately, Tupac Shakur's life was cut short on 1996 in Las Vegas. He changed an era with his lyrics and actions altering the course of the industry.
 
 
 
