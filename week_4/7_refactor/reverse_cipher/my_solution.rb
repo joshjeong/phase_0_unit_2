8a38dc80dbeaa49dc6f6d65293898ecaf5c31ab4
joshjeong <jcjeong@gmail.com>
2014-04-28 12:43:28 -0700

    Update my_solution.rb

--- week_4/7_refactor/reverse_cipher/my_solution.rb
+++ week_4/7_refactor/reverse_cipher/my_solution.rb
@@ -65,4 +65,12 @@ p translate_to_cipher("I want cookies")
 
 
 # 5. Reflection 
+# What parts of your strategy worked? What problems did you face? 
+# Going through each line item in the code was very effective.
+# What questions did you have while coding? What resources did you find to help you answer them?
+# What concepts are you having trouble with, or did you just figure something out? If so, what?
+# Did you learn any new skills or tricks?
+# How confident are you with each of the Learning Competencies?
+# Which parts of the challenge did you enjoy?
+# Which parts of the challenge did you find tedious?
 
