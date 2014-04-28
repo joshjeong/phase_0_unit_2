feafb27c9f7eb574bfcd950607d34ebc77179bc4
joshjeong <jcjeong@gmail.com>
2014-04-27 22:41:13 -0700

    Update my_solution.rb

--- week_4/4_nums_commas_solo_challenge/my_solution.rb
+++ week_4/4_nums_commas_solo_challenge/my_solution.rb
@@ -11,9 +11,34 @@
 # What is the output? (i.e. What should the code return?)
 # What are the steps needed to solve the problem?
 
+# define method separate_comma that passes one argument
+# convert argument input to a string
+# create condition thresholds for thousands, millions, and billions
+# insert comma into the correct spot
+# return the number now with comma(s)
 
 # 2. Initial Solution
 
+def separate_comma(input)
+  number= input.to_s
+  if number.length>9
+    number.insert(-3,',')
+    number.insert(-6,',')
+    number.insert(-9,',') 
+  elsif number.length>6
+  	number.insert(-3,',')
+    number.insert(-6,',')
+  elsif number.length>3
+  	number.insert(-3,',')
+  else
+  	return number
+end
+end
+
+
+
+# 3. Refactored Solution
+#the commas were in the wrong position
 def separate_comma(input)
   number= input.to_s
   if number.length>9
@@ -31,9 +56,18 @@ end
 end
 
 
-
-# 3. Refactored Solution
-
-
-
 # 4. Reflection 
+What parts of your strategy worked? What problems did you face?
+I had the right initial plan but my commas were a little off
+What questions did you have while coding? What resources did you find to help you answer them?
+I had to research a method that woiuld insert the comma
+What concepts are you having trouble with, or did you just figure something out? If so, what?
+I couldn't get the rspec to work- I kept getting an error regarding the describe.
+Did you learn any new skills or tricks?
+.insert
+How confident are you with each of the Learning Competencies?
+All good in the hood
+Which parts of the challenge did you enjoy?
+When I found out my initial solution was really close.
+Which parts of the challenge did you find tedious?
+None
