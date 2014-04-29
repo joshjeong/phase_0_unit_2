751570e3c5a607290bdda8d218f2da9b6c33d65c
Josh Jeong <jcjeong@gmail.com>
2014-04-29 13:11:00 -0700

    1 Die Solution

--- week_5/1_die/my_solution.rb
+++ week_5/1_die/my_solution.rb
@@ -5,31 +5,53 @@
 
 # 2. Pseudocode
 
-# Input:
-# Output:
-# Steps:
+# define class Die
+# define initialization that passes one argument sides
+# if sides is less than 1, raise an Argument ArgumentError
+# set instance variable to argument sides
+# define method sides which returns instance variable sides
+# define method roll that picks a random number from 1 to instance variable sides
 
 
 # 3. Initial Solution
 
 class Die
   def initialize(sides)
-    # code goes here
+  	if sides < 1
+  		raise ArgumentError
+  	end
+    @sides= sides
   end
   
   def sides
-    # code goes here
+  	return @sides 
   end
   
   def roll
-    # code goes here
+  	prng= Random.new
+  	prng.rand(1..@sides)
   end
 end
 
 
 
 # 4. Refactored Solution
-
+class Die
+  def initialize(sides)
+  	if sides < 1
+  		raise ArgumentError
+  	end
+    @sides= sides
+  end
+  
+  def sides
+  	return @sides 
+  end
+  
+  def roll
+  	(1..@sides).to_a.sample # found a way to select random number with one line
+  end						# it also made the code faster
+end
 
 
 
@@ -43,3 +65,19 @@ end
 
 
 # 5. Reflection 
+# What parts of your strategy worked? What problems did you face?
+# I kept getting one error: ArgumentError and wasn't sure how to pass it. I was unaware
+# of raise ArgumentError. 
+# What questions did you have while coding? What resources did you find to help you answer them?
+# I wanted to find a better way to select a random variable and remembered we used .sample
+# in a previous exercise.
+# What concepts are you having trouble with, or did you just figure something out? If so, what?
+# I'm still figuring out the different types of variables
+# Did you learn any new skills or tricks?
+# raise ArgumentError and instance var are new to me
+# How confident are you with each of the Learning Competencies?
+# Still not a little iffy on instance var
+# Which parts of the challenge did you enjoy?
+# Being able to translate my logic to code
+# Which parts of the challenge did you find tedious?
+# None
