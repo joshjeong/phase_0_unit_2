458ac1c9db40c83893a4e783788cec225b93df80
joshjeong <jcjeong@gmail.com>
2014-04-30 13:46:17 -0700

    Update my_solution.rb

--- week_5/4_boggle_board/my_solution.rb
+++ week_5/4_boggle_board/my_solution.rb
@@ -1,13 +1,32 @@
 # U2.W5: A Nested Array to Model a Boggle Board
 
-# I worked on this challenge [by myself, with: ].
+# I worked on this challenge [Phillip Smith].
 
 boggle_board = [["b", "r", "a", "e"],
                 ["i", "o", "d", "t"],
                 ["e", "c", "l", "r"],
                 ["t", "a", "k", "e"]]
 
+#Part 1
+  def create_word(board, *coords)
+    coords.map { |coord| board[coord.first][coord.last]}.join("")
+  end
 
+  puts create_word(boggle_board, [2,1], [1,1], [1,2], [0,3])  #=> returns "code"  
+  puts create_word(boggle_board, [0,1], [0,2], [1,2])  #=> creates what california slang word? RAD
+
+#Part 2
+def row(source, number)
+    source[number].to_a
+end
+
+
+#Part 3
+def col(source, numb)
+	source.map { |x| 
+		x[numb]
+	}
+end
 # Part 1: Access multiple elements of a nested array
 
 # Pseudocode
