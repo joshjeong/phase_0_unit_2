6f5a9020fe4c5280995d53becddd027598862cce
joshjeong <jcjeong@gmail.com>
2014-05-02 16:11:26 -0700

    Update my_solution.rb

--- week_5/6_validate_credit_card/my_solution.rb
+++ week_5/6_validate_credit_card/my_solution.rb
@@ -15,9 +15,39 @@
 # Don't forget to check on intialization for a card length
 # of exactly 16 digits
 class CreditCard
+  def initialize(cardNumb)
+  	@cardNumb= cardNumb.to_s
+    if @cardNumb.length != 16
+      raise ArgumentError
+  	end
+  end
+
+  def check_card
+    arr_modified=[]
+  	arr=@cardNumb.split("")
+    arr.each_with_index {|number, index|
+    if index%2 ==0
+      arr_modified << number.to_i*2
+    else 
+      arr_modified << number.to_i
+    end
+    }
+    tempString= Integer arr_modified.join
+    tempString2= String tempString
+    total= tempString2.split("")
+    sum= 0
+    total.each {|x|
+      sum+=x.to_i
+    }
+    if sum%10==0
+      return true
+    else 
+      false
+    end
+  end
+
 
 end
-
 
 
 # 4. Refactored Solution
