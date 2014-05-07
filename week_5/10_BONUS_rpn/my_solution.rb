a892e1b9ae5122f5db40ed930453daccf77868c7
joshjeong <jcjeong@gmail.com>
2014-05-07 10:45:48 -0700

    Update my_solution.rb

--- week_5/10_BONUS_rpn/my_solution.rb
+++ week_5/10_BONUS_rpn/my_solution.rb
@@ -1,5 +1,5 @@
 # U2.W5: Implement a Reverse Polish Notation Calculator
-
+#DID NOT FINISH
 
 # I worked on this challenge [by myself, with: ].
 
@@ -13,9 +13,24 @@
 # 3. Initial Solution
 
 class RPNCalculator
-  # Define your methods, here!
+
+  def bonus (equation)
+      arr = equation.split(" ")
+      arr1=[]
+      arr.each do |char|
+        if char.ord >= 42 && char.ord <= 47
+          math_sym= char.ord  
+                
+        else
+          arr1 << char.to_i
+        end
+      end
+      print arr1
+  end
 end
 
+
+bonus("5 4 +")
 
 
 # 4. Refactored Solution
